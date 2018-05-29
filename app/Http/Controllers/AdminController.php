d2866a2b61551515d9b10e66aa04aceda41ce95d
Tyler <tylereadams@gmail.com>
2018-05-29 19:50:01 -0400

    Show results on tweet log page.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,14 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\TeamsTweets;
+use App\TweetLogs;
 
 class AdminController extends Controller
 {
 
     public function tweetLog()
     {
-        $tweets = TeamsTweets::orderBy('created_at', 'DESC')->get();
-        $tweets->load('team');
+        $tweetLog = TweetLogs::orderBy('created_at', 'DESC')->get();
+        $tweetLog->load('team');
+
+        foreach($tweetLog as $tweet){
+            $tweets[] = [
+                'id' => $tweet->tweet_id,
+                'isInvalid' => $tweet->is_invalid,
+                'team' => [
+                  'twitter' => $tweet->team->twitter,
+                  'leagueId' => $tweet->team->league_id
+              ],
+            ];
+        }
 
         return view('admin.tweet-log', ['tweets' => $tweets]);
     }
--- resources/views/admin/tweet-log.blade.php
+++ resources/views/admin/tweet-log.blade.php
@@ -4,11 +4,15 @@
     <div class="container">
         <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
             @foreach($tweets as $tweet)
-                {!! Form::open(['url' => 'tweets/'.$tweet->tweet_id.'/save', 'method' => 'post']) !!}
+                {!! Form::open(['url' => 'tweets/'.$tweet['id'].'/save', 'method' => 'post']) !!}
                 <blockquote class="twitter-video" data-lang="en">
-                    <a href="https://twitter.com/{{ $tweet->team->twitter }}/status/{{ $tweet->tweet_id }}"></a>
+                    <a href="https://twitter.com/{{ $tweet['team']['twitter'] }}/status/{{ $tweet['id'] }}"></a>
                 </blockquote>
-                {!! Form::hidden('league', $tweet->team->league_id) !!}
+        <ul>
+            <li>{{ $tweet['isInvalid'] }}</li>
+            <li>{{ $tweet['team']['twitter'] }}</li>
+        </ul>
+                {!! Form::hidden('league', $tweet['team']['leagueId']) !!}
                 {!! Form::button('Good', array('type' => 'submit', 'name' => 'goodImage', 'class' => 'btn btn-success'))  !!}
                 {!! Form::button('Bad', array('type' => 'submit', 'name' => 'badImage', 'class' => 'btn btn-danger'))  !!}
                 {!! Form::close() !!}
