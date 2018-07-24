9b6aac3f06de5ce1ae782b98f375bb3a75676acb
Tyler <tylereadams@gmail.com>
2018-07-23 22:56:21 -0400

    Remove hashtag from tweet, looks weird.

--- app/Teams.php
+++ app/Teams.php
@@ -108,16 +108,13 @@ class Teams extends Model
 
             $mediaUrl = $tweet->extended_entities->media[0]->expanded_url;
 
-            // Set tweet hashtag, if it's the same as the team name don't add it
-            $tweetHashtag = (strtolower($this->nickname) == strtolower($this->hashtag) ? '' : '#'.$this->hashtag);
-
             echo "posting tweet ".$mediaUrl."\n";
             if (\App::environment('production'))
             {
                 // Post the tweet on production
                 Twitter::postTweet([
                     'status' => '#'.hashTagFormat($game->homeTeam->nickname).' '.$game->home_score.' #'.hashTagFormat($game->awayTeam->nickname).' '.$game->away_score.'                                               
-                               '.$tweetHashtag.'              '.$mediaUrl
+                                             '.$mediaUrl
                 ]);
             }
 
