0ab43f8991a52b5ab9b679bab2ad7276d0b0ee3a
Tyler <tylereadams@gmail.com>
2018-12-06 21:18:04 -0500

    Need to namespace Storage

--- app/TweetLogs.php
+++ app/TweetLogs.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use App\Services\StreamableService;
+use Illuminate\Support\Facades\Storage;
 
 class TweetLogs extends Model
 {
