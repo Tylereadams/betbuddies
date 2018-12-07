61671b60d70f864a2ffefa024034f4155a753973
Tyler <tylereadams@gmail.com>
2018-12-06 22:01:38 -0500

    Namespace StreamableService

--- app/TweetLogs.php
+++ app/TweetLogs.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Storage;
+use App\Services\StreamableService;
 
 class TweetLogs extends Model
 {
