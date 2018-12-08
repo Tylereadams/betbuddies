c6196634c4d15a6b319e142672770601a7188661
Tyler <tylereadams@gmail.com>
2018-12-07 21:45:08 -0500

    Set width to 100%

--- resources/views/partials/highlight.blade.php
+++ resources/views/partials/highlight.blade.php
@@ -1,4 +1,4 @@
-<video height="240" poster="{{ $imageUrl }}" controls>
+<video width="100%" height="240" poster="{{ $imageUrl }}" controls>
     <source src="{{ $highlightUrl }}" type="video/mp4">
     Your browser does not support the video tag.
 </video>
