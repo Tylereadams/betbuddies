8b9725983ef197706c5fe903e2694621eaaa85b7
Tyler <tylereadams@gmail.com>
2019-02-12 17:23:52 -0500

    Remove testing code

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -60,7 +60,6 @@ class AdminController extends Controller
         $teams = Teams::orderBy('nickname')->get();
         $teams->load(['league', 'credentials']);
 
-        dd(config('services.twitter'));
         $selectedTeam = $teams->first();
         if(Request::get('teamId')) {
             $selectedTeam = Teams::where('id', Request::get('teamId'))->first();
