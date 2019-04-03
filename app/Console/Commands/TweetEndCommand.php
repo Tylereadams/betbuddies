9926130058a0fe5424ad396dee734f3ceddfdc51
Tyler <tylereadams@gmail.com>
2019-04-02 22:35:26 -0400

    Temp bug fix.

--- app/Console/Commands/TweetEndCommand.php
+++ app/Console/Commands/TweetEndCommand.php
@@ -64,11 +64,11 @@ class TweetEndCommand extends Command
             }
 
             // Only send the ending tweet once, didn't want to save these tweets to DB so storing in cache for 72 hours if it got sent
-            Cache::remember('ending-tweet-'.$game->id, 60 * 72, function ()use($game) {
-                $game->homeTeam->sendEndTweet($game);
-                $game->awayTeam->sendEndTweet($game);
-                return true;
-            });
+//            Cache::remember('ending-tweet-'.$game->id, 60 * 72, function ()use($game) {
+//                $game->homeTeam->sendEndTweet($game);
+//                $game->awayTeam->sendEndTweet($game);
+//                return true;
+//            });
         }
     }
 }
